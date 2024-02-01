d6221e1db5b4c5d98e9a6a39c8a9a9187fac32e2
moooooora <omarmohammed271@gmail.com>
2024-02-01 11:29:22 +0200

    Functionality cart view for cart page

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import redirect, render
+from django.core.exceptions import ObjectDoesNotExist
+
 from store.models import Product
 from .models import Cart,CartItem
 
@@ -32,6 +34,18 @@ def add_cart(request,product_id):
     return redirect('cart:cart')       
 
 
-def cart(request):
-
-    return render(request,'store/cart.html')
+def cart(request,total=0,quantity=0,cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart,is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price) * (cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass        
+    context = {
+        'total':total,
+        'quantity':quantity,
+        'cart_items':cart_items,
+    }    
+    return render(request,'store/cart.html',context)
